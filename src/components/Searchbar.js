fbc3b9514700d2824df988884eef51aa4c2a6fa7
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2021-08-16 00:57:23 +0600

    commit change

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,31 +1,28 @@
-import React, { useState } from 'react'
-import youtube from '../apis/youtube';
+import React, { useState } from 'react';
 
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
 
+  const onSubmit = (event) => {
+    event.preventDefault();
 
-const Searchbar = ({onFormSubmit}) => {
-    const [term , setTerm] =useState('')
-    const onInputchange = event =>{
-        setTerm(event.term.value);
+    onFormSubmit(term);
+  };
 
-    };
-
-    const onSubmit = event => {
-       event.preventDefault();
-       onFormSubmit(term);
-    };
-    return (
-        <div className = "search-bar ui segment">
-        <form onSubmit={onSubmit} className="ui form">
-            <div className = "field">
-                <label style={{fontSize: '20px'}}>Video Search</label>
-                <input type = "text" 
-                value = {term}
-                onChange={onInputchange} />
-            </div>
-        </form>
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
     </div>
-    )
-}
+  );
+};
 
-export default Searchbar
+export default SearchBar;
